84b32ac1b9ec8b886982e837f0483d3bab157061
pamelaa <0000113685177xsp@al.educacao.sp.gov.br>
2024-07-30 17:41:28 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("blue");
+    
+    if (mouseIsPressed){
+     rect (mouseX, mouseY , 20, 35);
+    }
+    
+  }
+      
+    
